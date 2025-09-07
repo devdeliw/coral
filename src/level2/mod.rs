1314dd2a9a5587ba0066434583103c2a96495381
Deval Deliwala <vinland@Devals-MacBook-Pro.local>
2025-09-07 00:24:55 -0700

    mod.rs

--- src/level2/mod.rs
+++ src/level2/mod.rs
@@ -1,8 +1,22 @@
-pub mod trans;
+pub mod enums; 
 pub mod assert_length_helpers;
 
 pub mod sgemv;
 pub mod dgemv; 
 
+pub mod strsv; 
+pub mod dtrsv; 
 pub mod strusv;
 pub mod strlsv;
+pub mod dtrusv; 
+pub mod dtrlsv; 
+
+pub mod strmv; 
+pub mod dtrmv; 
+pub mod strumv; 
+pub mod strlmv; 
+pub mod dtrumv; 
+pub mod dtrlmv; 
+
+pub mod sger; 
+pub mod dger; 
