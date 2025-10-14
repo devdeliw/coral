f87bc343788765e926a00032a81eb8377297eeb9
devdeliw <devaldeliwala@berkeley.edu>
2025-10-13 19:39:39 -0700

    cgemm; not benched or tuned

---
+++ src/level3/cgemm.rs
@@ -0,0 +1,60 @@
+use crate::level3::{
+    cgemm_nn::cgemm_nn,
+    cgemm_nt::cgemm_nt,
+    cgemm_tn::cgemm_tn,
+    cgemm_tt::cgemm_tt,
+    cgemm_nc::cgemm_nc,
+    cgemm_tc::cgemm_tc,
+    cgemm_cn::cgemm_cn,
+    cgemm_ct::cgemm_ct,
+    cgemm_cc::cgemm_cc,
+};
+
+use crate::enums::CoralTranspose;
+use crate::level3::microkernel::c32_mrxnr::Complex32; 
+
+pub const MC: usize = 384;
+pub const NC: usize = 576;
+pub const KC: usize = 256;
+
+#[inline(always)]
+fn c32(x: [f32; 2]) -> Complex32 {
+    Complex32 { 
+        re: x[0], 
+        im: x[1]
+    }
+}
+
+#[inline(always)]
+pub fn cgemm(
+    op_a  : CoralTranspose,
+    op_b  : CoralTranspose,
+    m     : usize,
+    n     : usize,
+    k     : usize,
+    alpha : Complex32,
+    a     : *const f32,
+    lda   : usize,
+    b     : *const f32,
+    ldb   : usize,
+    beta  : [f32; 2],
+    c     : *mut f32,
+    ldc   : usize,
+) {
+    
+    alpha = c32(alpha); 
+    beta  = c32(beta); 
+
+    match (op_a, op_b) {
+        (CoralTranspose::NoTranspose,        CoralTranspose::NoTranspose)        => cgemm_nn(m, n, k, alpha, a, lda, b, ldb, beta, c, ldc),
+        (CoralTranspose::NoTranspose,        CoralTranspose::Transpose)          => cgemm_nt(m, n, k, alpha, a, lda, b, ldb, beta, c, ldc),
+        (CoralTranspose::Transpose,          CoralTranspose::NoTranspose)        => cgemm_tn(m, n, k, alpha, a, lda, b, ldb, beta, c, ldc),
+        (CoralTranspose::Transpose,          CoralTranspose::Transpose)          => cgemm_tt(m, n, k, alpha, a, lda, b, ldb, beta, c, ldc),
+        (CoralTranspose::NoTranspose,        CoralTranspose::ConjugateTranspose) => cgemm_nc(m, n, k, alpha, a, lda, b, ldb, beta, c, ldc),
+        (CoralTranspose::Transpose,          CoralTranspose::ConjugateTranspose) => cgemm_tc(m, n, k, alpha, a, lda, b, ldb, beta, c, ldc),
+        (CoralTranspose::ConjugateTranspose, CoralTranspose::NoTranspose)        => cgemm_cn(m, n, k, alpha, a, lda, b, ldb, beta, c, ldc),
+        (CoralTranspose::ConjugateTranspose, CoralTranspose::Transpose)          => cgemm_ct(m, n, k, alpha, a, lda, b, ldb, beta, c, ldc),
+        (CoralTranspose::ConjugateTranspose, CoralTranspose::ConjugateTranspose) => cgemm_cc(m, n, k, alpha, a, lda, b, ldb, beta, c, ldc),
+    }
+}
+
--- src/level3/mod.rs
+++ src/level3/mod.rs
@@ -22,3 +22,16 @@ pub(crate) mod dgemm_nt;
 pub(crate) mod dgemm_tn; 
 pub(crate) mod dgemm_tt;
 
+pub mod cgemm; 
+pub(crate) mod cgemm_nn;
+pub(crate) mod cgemm_nt; 
+pub(crate) mod cgemm_tn; 
+pub(crate) mod cgemm_tt;
+pub(crate) mod cgemm_nc;
+pub(crate) mod cgemm_tc;
+pub(crate) mod cgemm_cn;
+pub(crate) mod cgemm_ct;
+pub(crate) mod cgemm_cc;
+
+pub mod zgemm; 
+
