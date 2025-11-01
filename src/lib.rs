9008d8df76e460a4c381db43fc56a3553da1e04c
devdeliw <devaldeliwala@berkeley.edu>
2025-10-31 22:44:52 -0700

    lib

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,17 @@ compile_error!("coral-blas targets AArch64 only. \
 This build is running on a non-AArch64 target.");
 
 #[cfg(target_arch = "aarch64")]
-#[cfg_attr(docsrs, doc(cfg(target_arch = "aarch64")))]
 pub mod level1;
 
 #[cfg(target_arch = "aarch64")]
-#[cfg_attr(docsrs, doc(cfg(target_arch = "aarch64")))]
 pub mod level2;
 
 #[cfg(target_arch = "aarch64")]
-#[cfg_attr(docsrs, doc(cfg(target_arch = "aarch64")))]
 pub mod level3;
 
 pub mod enums;
 
 #[cfg(target_arch = "aarch64")]
-#[cfg_attr(docsrs, doc(cfg(target_arch = "aarch64")))]
 pub(crate) mod level1_special;
 
 
