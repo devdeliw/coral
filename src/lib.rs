db562b63d3677bb3774ffa41d5dd4d19a9454b4f
devdeliw <devaldeliwala@berkeley.edu>
2025-10-31 22:32:09 -0700

    aarch64 gating

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
-pub mod level1; 
+#[cfg(not(target_arch = "aarch64"))]
+compile_error!("coral-blas targets AArch64 only. \
+This build is running on a non-AArch64 target.");
+
+#[cfg(target_arch = "aarch64")]
+#[cfg_attr(docsrs, doc(cfg(target_arch = "aarch64")))]
+pub mod level1;
+
+#[cfg(target_arch = "aarch64")]
+#[cfg_attr(docsrs, doc(cfg(target_arch = "aarch64")))]
 pub mod level2;
+
+#[cfg(target_arch = "aarch64")]
+#[cfg_attr(docsrs, doc(cfg(target_arch = "aarch64")))]
 pub mod level3;
+
 pub mod enums;
+
 pub(crate) mod level1_special;
+
+
